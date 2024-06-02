9a4f1b14f397846e4b61721753adc4fb6f936bca
Paulo Martinez <paulomartinez1999@gmail.com>
2024-06-02 10:47:56 -0500

    agregado footer

---
+++ Wizard/src/components/footer.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <v-footer dark padless app inset >
+        <v-card class="flex" flat tile color="reed" :elevation="10">
+            <v-card-text class="py-2 white--text text-center">
+                © {{new Date().getFullYear()}}  - <strong>Paulo Martinez</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
--- Wizard/src/views/FormOTPView.vue
+++ Wizard/src/views/FormOTPView.vue
@@ -1,15 +1,19 @@
 <template>
-  <FormOTP />
+  <v-app>
+    <FormOTP />
+    <Footer></Footer>
+  </v-app>
 </template>
 
 <script>
   import FormOTP from '../components/FormOTP'
-
+  import Footer from '@/components/footer.vue';
   export default {
     name: 'Home',
 
     components: {
       FormOTP,
+      Footer,
     },
   }
 </script>
